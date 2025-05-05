e65583542755f80937d0a91e5b81ac2cde0823b8
shoxjaxon77 <shoxjahonatabayev77@gmail.com>
2025-05-05 17:02:04 +0500

    fix settings

--- build.sh
+++ build.sh
@@ -2,10 +2,17 @@
 # exit on error
 set -o errexit
 
+# Create static directory if it doesn't exist
+mkdir -p static
+
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Run migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
 
 # Start the application
 gunicorn config.wsgi:application
