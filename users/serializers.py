89fdd2c6f6113de859db86eca1855ab9ac1b8cd9
shoxjaxon77 <shoxjahonatabayev77@gmail.com>
2025-05-06 21:12:04 +0500

    Added user profile update functionality with password change

--- users/serializers.py
+++ users/serializers.py
@@ -2,10 +2,35 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(write_only=True, required=False)
+    new_password = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'phone_number')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'old_password', 'new_password')
         read_only_fields = ('id',)
+        extra_kwargs = {
+            'username': {'required': False},
+            'email': {'required': False}
+        }
+
+    def validate(self, data):
+        if 'new_password' in data and not data.get('old_password'):
+            raise serializers.ValidationError({'old_password': 'Old password is required to set new password'})
+        if 'old_password' in data and not data.get('new_password'):
+            raise serializers.ValidationError({'new_password': 'New password is required'})
+        return data
+
+    def update(self, instance, validated_data):
+        old_password = validated_data.pop('old_password', None)
+        new_password = validated_data.pop('new_password', None)
+
+        if old_password and new_password:
+            if not instance.check_password(old_password):
+                raise serializers.ValidationError({'old_password': 'Wrong password'})
+            instance.set_password(new_password)
+
+        return super().update(instance, validated_data)
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
--- users/views.py
+++ users/views.py
@@ -16,6 +16,22 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+            
+        # Save the instance if password was changed
+        if 'new_password' in request.data:
+            instance.save()
+            
+        return Response(serializer.data)
+
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def current_user(request):
